dad170e607539143c9806a27ec736965da3cc847
Liam Garrison <liamgarrison@gmail.com>
2019-02-28 15:23:12 +0000

    Setup password protected page for film

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 app.use(express.static("public"));
+app.use(express.urlencoded());
+// app.use(express.json());
+
 const port = process.env.PORT || 4000;
+
+// Load film
+app.post("/film", function(req, res) {
+  if (req.body.password == process.env.FILMPASSWORD) {
+    res.sendFile(path.join(__dirname + "/film/index.html"));
+  } else {
+    res.status(404).sendFile(path.join(__dirname + "/film/error.html"));
+  }
+});
+app.get("/film", function(req, res) {
+  res.status(404).sendFile(path.join(__dirname + "/film/error.html"));
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Minify images
@@ -12,7 +29,7 @@ compress_images(
   "public/images/",
   { compress_force: false, statistic: true, autoupdate: true },
   false,
-  { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+  { jpg: { engine: "mozjpeg", command: ["-quality", "70"] } },
   { png: { engine: "pngquant", command: ["--quality=20-50"] } },
   { svg: { engine: "svgo", command: "--multipass" } },
   { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
